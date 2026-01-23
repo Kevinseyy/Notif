2c682cafcc3eae0e854847df261167e025edbf2a
Kevin Sey <Seykevin6@gmail.com>
2026-01-23 16:56:44 +0100

    Creates new "tab"

--- Public/app.mjs
+++ Public/app.mjs
@@ -1,5 +1,18 @@
 const createGroupBtn = document.getElementById("createGroupBtn");
+const groupsList = document.getElementById("groupsList");
 
 createGroupBtn.addEventListener("click", () => {
   alert("Create Group clicked!");
 });
+
+function addGroupTab(group) {
+  const tab = document.createElement("div");
+  tab.className = "group-tab";
+
+  tab.innerHTML = `
+      <strong>${group.name}</strong>
+      <span>${group.memberCount} member</span>
+    `;
+
+  groupsList.prepend(tab);
+}
