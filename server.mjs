3dbaa77e595409ca2f7a2e535bab5135e2b25e63
Kevin Sey <Seykevin6@gmail.com>
2026-01-23 17:05:03 +0100

    Api router

--- server.mjs
+++ server.mjs
@@ -1,10 +1,13 @@
 import express from "express";
+import { apiRouter } from "./api.mjs";
 
 const PORT = 8080;
 const app = new express();
 
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/api/v1", apiRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello Class!");
